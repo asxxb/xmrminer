97d5a63fe2a908470163ed73cc2b912ebe362d5e
aseeb shibin <55710160+asxxb@users.noreply.github.com>
2023-10-11 12:34:52 +0530

    Add files via upload

---
+++ xmrig/start.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level 1 -o us.monero.herominers.com:1111 -u 441zyYuyGHTdHVnkgzP2tfUqyQKnXoXSmAzKneaYkuto5WwVRGdDVT1fm241m4LhfN9iiXxpGVZh2dQMBAL9uVUjQFDuXAb -p mycomputer -a rx/0 -k 
+pause
